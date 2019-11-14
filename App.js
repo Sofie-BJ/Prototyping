fc06e4bd96ca179787f7d6b2e6b6ff497f24592f
Sofie B. Jørgensen <sofie@Joergensen.local>
2019-11-14 11:39:58 +0100

    Primary structure added

--- App.js
+++ App.js
@@ -1,43 +1,58 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Home from './screens/Home';
+import CreateRoute from './screens/CreateRoute';
+import CreatePopUp from './components/CreatePopUp';
+import Menu from './components/Menu';
+import UpdatePopUp from './components/UpdatePopUp';
+import {createStackNavigator} from 'react-navigation';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
+
+//StackNavigator. Define screens and initialRouteName specifies start screen. 
+const StackNavigator = createStackNavigator({
+    Home,
+    CreateRoute
+}, {
+    initialRouteName: 'Home'
 });
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <Text style={styles.instructions}>To get started, edit App.js</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
 
-        <Text>tHEA KAN LIDE TE</Text>
+                <Text>tHEA KAN LIDE TE</Text>
 
-        <Text>Dette er ny linje - lad os teste det</Text>
+                <Text>Dette er ny linje - lad os teste det</Text>
 
-      </View>
-    );
-  }
+            </View>
+        );
+    }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
