069054d2ed2e23b633d7087d70cd93a1358986cd
Thea Schmidt <theaschmidt@Theas-MacBook-Pro-2.local>
2019-11-28 13:08:09 +0100

    Ændredet UpdatePopUp

--- src/components/CreatePopUp.js
+++ src/components/CreatePopUp.js
@@ -27,3 +27,4 @@ export default class CreatePopUp extends React.Component {
 
 
 
+
---
+++ src/components/UpdatePopUp.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import Route from "../Route"
+import RoutePoint from "../RoutePoint"
+
+
+import Dialog from "react-native-dialog";
+
+export default class CreatePopUp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.newRouteTitle = null;
+    }
+
+render () {
+    return (
+      <View>
+        <Dialog.Container visible={true}>
+          <Dialog.Title>Change RoutePoint</Dialog.Title>
+          <Dialog.Input placeholder="Write the title of what you wish to remember"/>
+          <Dialog.Button label="Cancel" onPress={() => this.props.cancel()}/>
+          <Dialog.Button label="Save" onPress={() => this.props.callback(this.newRouteTitle)}/>
+        </Dialog.Container>
+      </View>
+    )
+  };
+
+}
+
+  /*Route.map(RoutePoint.index)*/ 
