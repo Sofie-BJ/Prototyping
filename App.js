4056f9cb7b11b22238147b02f64d03136f08382b
Sofie B. Jørgensen <sofie@Joergensen.users.kollegie.dk>
2019-12-06 22:00:48 +0100

    icon added

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import Home from './src/screens/Home';
 import CameraScreen from './src/screens/CameraScreen';
-import CreatePopUp from './src/components/CreatePopUp';
-import Menu from './src/components/Menu';
-import UpdatePopUp from './src/components/UpdatePopUp';
 
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 import HomeRoutes from "./src/screens/HomeRoutes";
-import DisplayRoute from "./src/screens/DisplayRoute";
 import RouteCreator from "./src/screens/RouteCreator";
 import GoRoute from "./src/screens/GoRoute";
-import Route from "./src/Route";
 
 //StackNavigator. Define screens and initialRouteName specifies start screen.
 const StackNavigator = createStackNavigator(
@@ -21,7 +14,6 @@ const StackNavigator = createStackNavigator(
         CameraScreen: CameraScreen,
         RouteCreator: RouteCreator,
         GoRoute: GoRoute,
-        Route: Route
     },
     {
         initialRouteName: 'HomeRoutes'
