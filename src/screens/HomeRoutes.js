a2b23577a5b7f173690519c78569588c897a3eae
Sofie B. Jørgensen <sofie@Joergensen.local>
2019-12-10 14:16:28 +0100

    Delete routes

--- src/screens/HomeRoutes.js
+++ src/screens/HomeRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button, AsyncStorage, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, Button, AsyncStorage, TouchableOpacity, Alert} from 'react-native';
 import Route from "../Route";
 import CreatePopUp from "../components/CreatePopUp";
 import {IconButton, Colors} from 'react-native-paper';
@@ -19,9 +19,9 @@ export default class HomeRoutes extends React.Component {
     }
 
     componentDidMount() {
-        const { navigation } = this.props;
+        const {navigation} = this.props;
         this.focusListener = navigation.addListener('didFocus', () => {
-           this.getAllRoutes();
+            this.getAllRoutes();
         });
     }
 
@@ -39,6 +39,22 @@ export default class HomeRoutes extends React.Component {
         })
     };
 
+    deleteRoute = (routeTitle) => {
+        Alert.alert(
+            'Slet rute',
+            'Er du sikker på at du vil slette ruten?',
+            [
+                {
+                    text: 'OK', onPress: () => {
+                        AsyncStorage.removeItem(routeTitle).then(console.log("Item removed"));
+                        this.getAllRoutes();
+                    }
+                },
+                {text: 'Cancel', onPress: () => console.log("Cancel")},
+            ],
+            {cancelable: false},
+        );
+    };
 
     render() {
         let routes = this.state.routes;
@@ -63,7 +79,7 @@ export default class HomeRoutes extends React.Component {
                                     style={styles.walkingMan}
                                 />
                                 <IconButton
-                                    onPress={() => this.props.navigation.navigate("GoRoute", {routeTitle: routeTitle})}
+                                    onPress={() => this.deleteRoute(routeTitle)}
                                     icon='delete'
                                     size={30}
                                 />
@@ -105,7 +121,7 @@ const styles = StyleSheet.create({
         margin: 5,
         borderRadius: 10,
         width: '100%',
-    
+
     },
     text: {
         fontSize: 24,
